cda866cf68845fc0394815cd1ef7ba5adda6c16d
alpertSnow <rmddxx@163.com>
2017-11-11 20:52:12 +0800

    01params init

---
+++ 01params.R
@@ -0,0 +1,8 @@
+############### STE_wind_JU2003 ###############
+### 01params                                ###
+### load pro and obs data                   ###
+### set Bayesian inference parameters       ###
+### Date: 2017-11-11                        ###
+### By: xf                                  ###
+###############################################
+
