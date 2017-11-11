c936770abbfe657e03d85fb63f1a1c89bcf28a4a
alpertSnow <rmddxx@163.com>
2017-11-11 21:06:30 +0800

    read pre and obs data

--- 01params.R
+++ 01params.R
@@ -1,8 +1,29 @@
 ############### STE_wind_JU2003 ###############
 ### 01params                                ###
-### load pro and obs data                   ###
+### load pre and obs data                   ###
 ### set Bayesian inference parameters       ###
 ### Date: 2017-11-11                        ###
 ### By: xf                                  ###
 ###############################################
+
+## header
+
+## settings
+f.Ux.pre <- 'Ux_pre.csv'
+f.Uy.pre <- 'Uy_pre.csv'
+f.Uz.pre <- 'Uz_pre.csv'
+f.k.pre <- 'k_pre.csv'
+f.Ux.obs.mean <- 'Ux_obs_mean_PWIDS.csv'
+f.Uy.obs.mean <- 'Uy_obs_mean_PWIDS.csv'
+f.Ux.obs.sd <- 'Ux_obs_sd_PWIDS.csv'
+f.Uy.obs.sd <- 'Uy_obs_sd_PWIDS.csv'
+
+## read pre and obs data
+Ux.pre <- read.csv(f.Ux.pre, row.names = 1)
+Uy.pre <- read.csv(f.Uy.pre, row.names = 1)
+Ux.obs.mean <- read.csv(f.Ux.obs.mean, row.names = 1)
+Uy.obs.mean <- read.csv(f.Uy.obs.mean, row.names = 1)
+Ux.obs.sd <- read.csv(f.Ux.obs.sd, row.names = 1)
+Uy.obs.sd <- read.csv(f.Uy.obs.sd, row.names = 1)
+n.is.na.PWIDS <- which(is.na(Ux.obs.mean) & is.na(Uy.obs.mean) & is.na(Ux.obs.sd) & is.na(Uy.obs.sd))
 
