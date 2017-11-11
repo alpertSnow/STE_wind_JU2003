78e9bdf073597f52919054c8919453dd23dc0467
alpertSnow <rmddxx@163.com>
2017-11-11 22:26:36 +0800

    run JAGS model

--- 03runJags.R
+++ 03runJags.R
@@ -5,3 +5,17 @@
 ### By: xf                                  ###
 ###############################################
 
+## header
+library(R2jags)
+library(coda)
+
+## settings
+data <- list('n.valid.Ux', 'n.valid.Uy', 'Ux.map', 'Uy.map', 'Ux.mu', 'Uy.mu', 'Ux.tau', 'Uy.tau',
+             'U.ref', 'p.wdir', 'wdir.Cat', 'logWspdLower', 'logWspdUpper', 'wdirs')
+parameters <- c('wdir', 'wspd')
+inits <- NULL
+
+wf.sim <- jags(data, inits, parameters, 'windfinder.bug', n.chains = 3, n.iter = 2000, n.burnin = 0, n.thin = 1)
+
+wf.coda <- as.mcmc(wf.sim)
+print(wf.sim)
