4b234372c524ffdcbc344e0c2d4780d76243748a
alpertSnow <rmddxx@163.com>
2017-11-13 21:45:30 +0800

    output the inverse of covarience matrix

--- 01params.R
+++ 01params.R
@@ -39,7 +39,7 @@ Uy.obs.list <- lapply(Uy.obs[, -i.PWIDS.ignore], as.vector)
 U.obs.list <- mapply(cbind, Ux.obs.list, Uy.obs.list, SIMPLIFY = FALSE)
 L1 <- lapply(U.obs.list, cov, use = 'complete.obs')
 L2 <- lapply(L1, inv)
-Ucov.obs.array <- array(unlist(L2), dim = c(nrow(L2[[1]]), ncol(L2[[1]]), length(L2))) # array of covarience matrixs
+U.T.obs.array <- array(unlist(L2), dim = c(nrow(L2[[1]]), ncol(L2[[1]]), length(L2))) # array of inverse covarience matrixs
 
 # delete in pre
 Ux.map <- Ux.pre[,1:n.PWIDS][,-i.PWIDS.na][,-i.PWIDS.ignore]
