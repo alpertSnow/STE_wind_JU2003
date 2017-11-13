f26c2cc7ae2526f513edb0879129fc5c6c8241e1
alpertSnow <rmddxx@163.com>
2017-11-13 21:42:22 +0800

    calculate covarience as array

--- 01params.R
+++ 01params.R
@@ -7,6 +7,7 @@
 ###############################################
 
 ## header
+library(MASS)
 
 ## settings
 U.ref <- 6.13 # reference wind speed in CFD at H_ref = 30 m
@@ -14,46 +15,43 @@ f.Ux.pre <- 'Ux_pre.csv'
 f.Uy.pre <- 'Uy_pre.csv'
 f.Uz.pre <- 'Uz_pre.csv'
 f.k.pre <- 'k_pre.csv'
-f.Ux.obs.mean <- 'Ux_obs_mean_PWIDS.csv'
-f.Uy.obs.mean <- 'Uy_obs_mean_PWIDS.csv'
-f.Ux.obs.sd <- 'Ux_obs_sd_PWIDS.csv'
-f.Uy.obs.sd <- 'Uy_obs_sd_PWIDS.csv'
+f.Ux.obs <- 'Ux_obs_PWIDS.csv'
+f.Uy.obs <- 'Uy_obs_PWIDS.csv'
 n.PWIDS <- 15
-i.PWIDS.ignore <- 15 # ignore PWIDS15 for validation
+i.PWIDS.ignore <- 14 # ignore PWIDS15 for validation
+i.PWIDS.na <- 9 # drop PWIDS09 (no valid data)
 
 ## read pre and obs data
 Ux.pre <- read.csv(f.Ux.pre, row.names = 1)
 Uy.pre <- read.csv(f.Uy.pre, row.names = 1)
 k.pre <- read.csv(f.k.pre, row.names = 1)
-Ux.obs.mean <- read.csv(f.Ux.obs.mean, row.names = 1)
-Uy.obs.mean <- read.csv(f.Uy.obs.mean, row.names = 1)
-Ux.obs.sd <- read.csv(f.Ux.obs.sd, row.names = 1)
-Uy.obs.sd <- read.csv(f.Uy.obs.sd, row.names = 1)
+Ux.obs <- read.csv(f.Ux.obs, row.names = 1)
+Uy.obs <- read.csv(f.Uy.obs, row.names = 1)
 
-## process pre abd obs data
-# delete the NA location in obs
-i.Ux.is.na.PWIDS <- which(is.na(Ux.obs.mean) & is.na(Ux.obs.sd))
-Ux.mu <- Ux.obs.mean[-c(i.Ux.is.na.PWIDS,i.PWIDS.ignore),]
-Ux.R <- Ux.obs.sd[-c(i.Ux.is.na.PWIDS,i.PWIDS.ignore),]^2
-Ux.tau <- 1/Ux.R
+## process pre abd obs data -> mu & covMat (array)
+# delete the ignored location in obs
+Ux.mu <- colMeans(Ux.obs[, -i.PWIDS.ignore])
+Uy.mu <- colMeans(Uy.obs[, -i.PWIDS.ignore])
+U.mu <- rbind(Ux.mu, Uy.mu)
 
-i.Uy.is.na.PWIDS <- which(is.na(Uy.obs.mean) & is.na(Uy.obs.sd))
-Uy.mu <- Uy.obs.mean[-c(i.Uy.is.na.PWIDS,i.PWIDS.ignore),]
-Uy.R <- Uy.obs.sd[-c(i.Uy.is.na.PWIDS,i.PWIDS.ignore),]^2
-Uy.tau <- 1/Uy.R
+Ux.obs.list <- lapply(Ux.obs[, -i.PWIDS.ignore], as.vector)
+Uy.obs.list <- lapply(Uy.obs[, -i.PWIDS.ignore], as.vector)
+U.obs.list <- mapply(cbind, Ux.obs.list, Uy.obs.list, SIMPLIFY = FALSE)
+L1 <- lapply(U.obs.list, cov, use = 'complete.obs')
+L2 <- lapply(L1, inv)
+Ucov.obs.array <- array(unlist(L2), dim = c(nrow(L2[[1]]), ncol(L2[[1]]), length(L2))) # array of covarience matrixs
 
 # delete in pre
-Ux.map <- Ux.pre[,1:n.PWIDS][,-c(i.Ux.is.na.PWIDS,i.PWIDS.ignore)]
-Uy.map <- Uy.pre[,1:n.PWIDS][,-c(i.Uy.is.na.PWIDS,i.PWIDS.ignore)]
-k.map <- k.pre[,1:n.PWIDS][,-c(i.Uy.is.na.PWIDS,i.PWIDS.ignore)]
+Ux.map <- Ux.pre[,1:n.PWIDS][,-i.PWIDS.na][,-i.PWIDS.ignore]
+Uy.map <- Uy.pre[,1:n.PWIDS][,-i.PWIDS.na][,-i.PWIDS.ignore]
+k.map <- k.pre[,1:n.PWIDS][,-i.PWIDS.na][,-i.PWIDS.ignore]
 
 # wind direction info
 wdirs <- as.numeric(row.names(Ux.pre))
 n.wdir <- length(wdirs)
 
 ## priors for wind direction and relative wind speed
-n.valid.Ux <- length(Ux.mu)
-n.valid.Uy <- length(Uy.mu)
+n.valid.U <- length(U.mu)
 wdir.Cat <- 1:n.wdir
 p.wdir <- rep(1,n.wdir)
 logWspdUpper <- 2
