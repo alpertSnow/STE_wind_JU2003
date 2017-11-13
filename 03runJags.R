6c04ba670e0f959462e625f9663186cfa8fa6a58
alpertSnow <rmddxx@163.com>
2017-11-13 14:13:29 +0800

    add: i.wdir as a output parameter

--- 03runJags.R
+++ 03runJags.R
@@ -12,10 +12,10 @@ library(coda)
 ## settings
 data <- list('n.valid.Ux', 'n.valid.Uy', 'Ux.map', 'Uy.map', 'Ux.mu', 'Uy.mu', 'Ux.tau', 'Uy.tau',
              'U.ref', 'p.wdir', 'wdir.Cat', 'logWspdLower', 'logWspdUpper', 'wdirs')
-parameters <- c('wdir', 'wspd')
+parameters <- c('wdir', 'wspd', 'i.wdir')
 inits <- NULL
 
-wf.sim <- jags(data, inits, parameters, 'windfinder.bug', n.chains = 3, n.iter = 2000, n.burnin = 0, n.thin = 1)
+wf.sim <- jags(data, inits, parameters, 'windfinder.bug', n.chains = 3, n.iter = 2000, n.burnin = 1000, n.thin = 1)
 
 wf.coda <- as.mcmc(wf.sim)
 print(wf.sim)
