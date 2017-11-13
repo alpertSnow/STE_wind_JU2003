af74c19c721658c1120a3354bb4eae886886e1eb
alpertSnow <rmddxx@163.com>
2017-11-13 15:55:59 +0800

    delete: posterior predicted test

--- 04postprocess.R
+++ 04postprocess.R
@@ -32,14 +32,3 @@ hist( mcmc$wdir, 20, col=rgb(1,0,0,1/4), xlim=c(130,220), freq = FALSE, add=T)  
 # hists of speed
 hist( wspd.obs[,14], 20, col=rgb(0,0,1,1/4), xlim=c(0, 15), freq = FALSE)  # first histogram
 hist( mcmc$wspd, 20, col=rgb(1,0,0,1/4), xlim=c(0, 15), freq = FALSE, add=T)  # second
-
-# posterior prective test
-wdir.post.mean <- mean(mcmc$wdir)
-wspd.post.mean <- mean(mcmc$wspd)
-Ux.post.mean <- Ux.map['178',] * wspd.post.mean / U.ref
-Uy.post.mean <- Uy.map['178',] * wspd.post.mean / U.ref
-mydata <- data.frame(c(t(Ux.post.mean), Ux.mu), c(t(Uy.post.mean), Uy.mu))
-colnames(mydata) <- c('Ux', 'Uy')
-mydata$PWIDS <- rep(colnames(Ux.map), 2)
-mydata$type <- c(rep('posterior',n.valid.Ux), rep('measured', n.valid.Ux))
-xyplot(Ux~Uy|PWIDS, mydata, groups = type, xlim = c(-4,4), ylim = c(-4,4))
