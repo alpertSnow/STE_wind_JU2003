4ecb60388f1bd033ef731e3063e217718e1ade04
alpertSnow <rmddxx@163.com>
2017-11-13 19:28:17 +0800

    test multivariate distributions

---
+++ mvnormTest.R
@@ -0,0 +1,23 @@
+## test sd of widr and wspd, Ux and Uy
+library(MASS)
+library(ggplot2)
+i.sensor <- 8
+U.obs.i <- data.frame(x=Ux.obs[,i.sensor], y=Uy.obs[,i.sensor])
+
+# Ux and Uy
+mean <- colMeans(U.obs.i)
+covMat <- cov(U.obs.i)
+sd1 <- sd(U.obs.i[,1])
+sd2 <- sd(U.obs.i[,2])
+U <- mvrnorm(1000, mean, covMat)
+Ux <- rnorm(1000, mean[1], sd1)
+Uy <- rnorm(1000, mean[2], sd2)
+
+df <- data.frame(U,Ux,Uy)
+g2 <- ggplot() +
+        geom_point(data = U.obs.i, aes(x, y), col = obs.col, alpha = 0.5) +
+        geom_point(data = df, aes(x, y), col = 'red', alpha = 0.3) +
+        geom_point(data = df, aes(Ux, Uy), col = 'green', alpha = 0.3) +
+        scale_x_continuous(limits = c(-6,6)) +
+        scale_y_continuous(limits = c(-6,6))
+plot(g2)
